fc5de2bc1b05f503166f4a27c853507df55bf11e
yopplop <theomae.dom@gmail.com>
2025-03-12 19:01:41 +0100

    good

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
-define('DB_PORT', '3306'); // Port MySQL par défaut pour MAMP
+define('DB_PORT', '8889'); // Port MySQL par défaut pour MAMP
 define('DB_USER', 'root');
 define('DB_PASS', 'root'); // Mot de passe par défaut pour MAMP
-define('DB_NAME', 'omnes_immobilier'); ?>
+define('DB_NAME', 'omnes_immobilier'); ?>   
